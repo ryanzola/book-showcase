3fc7b26692ab02115826e496a36624efd433fd52
ryanzola <ryanzola@me.com>
2019-07-07 11:52:47 -0400

    Red
    - Implements red header on home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
     
-    <h1>Hello, world!</h1>
+    <h1>WHAT CHU KNOW BOUT THESE COOL BOOKS?</h1>
    
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
@@ -26,7 +26,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+h1 {
+  color: red;
+}
+
 .home-links a {
   margin-right: 1rem;
 }
